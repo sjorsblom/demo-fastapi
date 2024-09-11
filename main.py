d68b70f785c1c5d4d92382ad56dbdb1e5a79c7da
sjorsblom <info@sjorsblom.com>
2024-09-11 12:55:39 +0200

    added isort and mypy to setup

--- main.py
+++ main.py
@@ -1,21 +1,15 @@
+from enum import Enum
+
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
 @app.get("/")
 async def root():
+    """Only single line comments are supported in FastAPI."""
     return {"message": "Hello World"}
-
-
-@app.get("/items/{item_id}")
-async def read_item(item_id: int):
-    return {"item_id": item_id}
-
-
-@app.get("/users/me")
-async def read_user_me():
-    return {"user_id": "the current user"}
 
 
 @app.get("/users/{user_id}")
@@ -25,5 +19,51 @@ async def read_user(user_id: str):
 
 @app.get("/users/other/{user_id}")
 async def read_another_user(user_id: str):
-    hi = 2
     return {"user_id": user_id}
+
+
+@app.get("/files/{file_path:path}")
+async def read_file(file_path: str):
+    """The path parameter type will help you match any path, including /, /path/to/file.txt, etc."""
+    return {"file_path": file_path}
+
+
+class ModelName(str, Enum):
+    ALEXNET = "alexnet"
+    RESNET = "resnet"
+    LENET = "lenet"
+
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name is ModelName.ALEXNET:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}
+
+
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+
+@app.get("/items/")
+async def read_item(skip: int = 0, limit: int = 10):
+    """Adding default values will automatically be made query parameters."""
+    return fake_items_db[skip : skip + limit]
+
+
+class Item(BaseModel):
+    """Pydantic models will be used to validate the request body."""
+
+    name: str
+    description: str | None = None
+    price: float
+    tax: int | None = None
+
+
+@app.post("/items/")
+async def create_item(item: Item):
+    some_value = item.name
+    return some_value
